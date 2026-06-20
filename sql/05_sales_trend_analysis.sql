277da8b7ec924d7d68cab2f071f4608cd7cb7b96
eclipsseii <qmuyfi@gmail.com>
2026-06-20 17:52:50 +0300

    Update 05_sales_trend_analysis.sql

--- sql/05_sales_trend_analysis.sql
+++ sql/05_sales_trend_analysis.sql
@@ -9,3 +9,15 @@ SELECT
 FROM olist_orders_dataset
 GROUP BY order_month
 ORDER BY order_month;
+
+
+-- Revenue by month
+
+SELECT
+    DATE_TRUNC('month', o.order_purchase_timestamp::timestamp) AS order_month,
+    ROUND(SUM(op.payment_value)::numeric, 2) AS revenue
+FROM olist_orders_dataset o
+JOIN olist_order_payments_dataset op
+    ON o.order_id = op.order_id
+GROUP BY order_month
+ORDER BY order_month;
