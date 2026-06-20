9372a068b661670f05fee09d873f02852f521625
eclipsseii <qmuyfi@gmail.com>
2026-06-20 16:26:34 +0300

    Update 04_revenue_analysis.sql

--- sql/04_revenue_analysis.sql
+++ sql/04_revenue_analysis.sql
@@ -16,3 +16,24 @@ JOIN olist_order_payments_dataset op
 GROUP BY t.product_category_name_english
 ORDER BY revenue DESC
 LIMIT 10;
+
+
+-- Revenue per item sold
+
+SELECT
+    t.product_category_name_english,
+    ROUND(
+        SUM(op.payment_value)::numeric / COUNT(*),
+        2
+    ) AS revenue_per_item
+FROM olist_order_items_dataset oi
+JOIN olist_products_dataset p
+    ON oi.product_id = p.product_id
+JOIN product_category_name_translation t
+    ON p.product_category_name = t.product_category_name
+JOIN olist_order_payments_dataset op
+    ON oi.order_id = op.order_id
+GROUP BY t.product_category_name_english
+HAVING COUNT(*) > 100
+ORDER BY revenue_per_item DESC
+LIMIT 10;
